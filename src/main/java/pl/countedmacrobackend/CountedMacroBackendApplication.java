d7fdcabfc0e8a308610c599b316f3b03ca4d6b81
szejnaArtur <50076342+szejnaArtur@users.noreply.github.com>
2022-10-19 17:38:23 +0200

    Start the backend app project.

---
+++ src/main/java/pl/countedmacrobackend/CountedMacroBackendApplication.java
@@ -0,0 +1,21 @@
+package pl.countedmacrobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class CountedMacroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountedMacroBackendApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
